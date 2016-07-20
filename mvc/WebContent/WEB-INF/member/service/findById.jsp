57f254a2031e43f5c15a2efa32eaa5c337d12192
xornjseh1000@gmail.com <xornjseh1000@gmail.com>
2016-07-20 10:32:37 +0900

    .

--- mvc/WebContent/WEB-INF/member/service/findById.jsp
+++ mvc/WebContent/WEB-INF/member/service/findById.jsp
@@ -22,7 +22,7 @@
 				<table id="member_detail">
 			<tr>
 				<td rowspan="3" style="width:30%">
-					<img src="<%=application.getContextPath() %>/img/f32w852ly0mu1zuryl72.jpg"  width="130" height="150">
+					<img src="<%=application.getContextPath()%>/img/bbo.jpg"  width="130" height="150">
 				</td>
 				<td style="width:20%" class="font_bold bg_color_yellow">ID</td>
 				<td style="width:40%"></td>
--- mvc/src/home/HomeController.java
+++ mvc/src/home/HomeController.java
@@ -12,23 +12,21 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class HomeController
  */
-@WebServlet("/home.do")
+@WebServlet({"/home.do","/top.do","/header.do","/footer.do","/end.do"})
 public class HomeController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public HomeController() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
+    
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		RequestDispatcher dis = request.getRequestDispatcher("/WEB-INF/global/home.jsp");
+		
+		System.out.println("서블릿 입장..");
+		String servletPath = request.getServletPath();
+		System.out.println("서블릿 경로"+servletPath);
+		String path = servletPath.split("/")[1];
+		String view = path.substring(0,path.indexOf("."));
+		System.out.println("가야할 경로:" +view);
+		
+		RequestDispatcher dis = request.getRequestDispatcher("/WEB-INF/global"+view+".jsp");
 		dis.forward(request, response);
 	}
 
